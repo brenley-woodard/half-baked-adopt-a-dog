c8f784e0d0388007925412eaa06f2fbb56e4fbf6
brenley-woodard <brenleyew@gmail.com>
2022-11-14 12:56:51 -0800

    detail.js gets the id from URL and fetched dog from supabase (verified in dev tool>network>name>preview)

--- detail/detail.js
+++ detail/detail.js
@@ -6,6 +6,9 @@ const dogDetailContainer = document.getElementById('dog-detail-container');
 // on load
 window.addEventListener('load', async () => {
     // get the id from URL
+    const detail = new URLSearchParams(window.location.search);
+    const id = detail.get('id');
     // use the id to fetch the dog
+    const dog = await getDog(id);
     // render and append this dog's details to the container
 });
